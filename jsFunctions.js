b7c2f9d269d834bfd7f1ecc02ecac814f1f4b81c
exelix <ex.elix@libero.it>
2018-12-18 12:37:42 +0100

    Update website to 2.3

--- jsFunctions.js
+++ jsFunctions.js
@@ -1,1 +1,61 @@
-function JsSZSRead(e){if(e[0].name.endsWith("szs")){var n=new FileReader;n.onloadend=function(e){SwitchThemesOnline.App.UploadSZS(new Uint8Array(e.target.result))},void 0!==e[0]&&n.readAsArrayBuffer(e[0])}else alert("This is not an szs file")}function JsDDSRead(e){if(e[0].name.endsWith("dds")){var n=new FileReader;n.onloadend=function(n){SwitchThemesOnline.App.UploadDDS(new Uint8Array(n.target.result),e[0].name)},void 0!==e[0]&&n.readAsArrayBuffer(e[0])}else alert("Only dds files are supported")}function JsThemePartRead(e){if(e[0].name.endsWith("szs")){var n=new FileReader;n.onloadend=function(e){SwitchThemesOnline.App.AutoThemeFileUploaded(new Uint8Array(e.target.result))},void 0!==e[0]&&n.readAsArrayBuffer(e[0])}else alert("This is not an szs file")}downloadBlob=function(e,n,a){var t,d;t=new Blob([e],{type:a}),d=window.URL.createObjectURL(t),downloadURL(d,n,a),setTimeout(function(){return window.URL.revokeObjectURL(d)},1e3)},downloadURL=function(e,n){var a;(a=document.createElement("a")).href=e,a.download=n,document.body.appendChild(a),a.style="display: none",a.click(),a.remove()};
+﻿function JsSZSRead(ev) {
+    if (!ev[0].name.endsWith('szs')) {
+        alert('This is not an szs file');
+        return;
+    }
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+        SwitchThemesOnline.App.UploadSZS(new Uint8Array(evt.target.result));
+    }
+    if (typeof ev[0] != 'undefined')
+        reader.readAsArrayBuffer(ev[0]);
+}
+
+function JsDDSRead(ev) {
+    if (!ev[0].name.endsWith('dds')) {
+        alert('Only dds files are supported');
+        return;
+    }
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+            SwitchThemesOnline.App.UploadDDS(new Uint8Array(evt.target.result), ev[0].name);
+    }
+    if (typeof ev[0] != 'undefined')
+        reader.readAsArrayBuffer(ev[0]);
+}
+
+function JsThemePartRead(ev) {
+    if (!ev[0].name.endsWith('szs')) {
+        alert('This is not an szs file');
+        return;
+    }
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+        SwitchThemesOnline.App.AutoThemeFileUploaded(new Uint8Array(evt.target.result));
+    }
+    if (typeof ev[0] != 'undefined')
+        reader.readAsArrayBuffer(ev[0]);
+}
+
+downloadBlob = function (data, fileName, mimeType) {
+    var blob, url;
+    blob = new Blob([data], {
+        type: mimeType
+    });
+    url = window.URL.createObjectURL(blob);
+    downloadURL(url, fileName, mimeType);
+    setTimeout(function () {
+        return window.URL.revokeObjectURL(url);
+    }, 1000);
+};
+
+downloadURL = function (data, fileName) {
+    var a;
+    a = document.createElement('a');
+    a.href = data;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.style = 'display: none';
+    a.click();
+    a.remove();
+};
