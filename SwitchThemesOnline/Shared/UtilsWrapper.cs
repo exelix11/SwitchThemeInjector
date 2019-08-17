f912709649a9dc63d14bf5209c9ca5998fe47c37
exelix <13405476+exelix11@users.noreply.github.com>
2019-08-18 00:23:10 +0200

    [Online] Download files without having to convert to base64 first

--- SwitchThemesOnline/Shared/UtilsWrapper.cs
+++ SwitchThemesOnline/Shared/UtilsWrapper.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using Mono.WebAssembly.Interop;
 using System;
 using System.Threading.Tasks;
 
 public static class Exten
 {
-	public static async void Click(this IJSRuntime runtime, ElementReference element) =>
+	public static async Task Click(this IJSRuntime runtime, ElementReference element) =>
 		await runtime.InvokeAsync<object>("DomUtil.Click", element);
 
-	public static async void Alert(this IJSRuntime runtime, string message) =>
+	public static async Task Alert(this IJSRuntime runtime, string message) =>
 		await runtime.InvokeAsync<object>("alert", message);
 
-	public static Task DownloadFile(this IJSRuntime runtime, string filename, byte[] data)
-		=> runtime.InvokeAsync<object>("saveAsFile", filename, Convert.ToBase64String(data));
+	public static void DownloadFile(this MonoWebAssemblyJSRuntime js, string fileName, byte[] Data) =>
+		js.InvokeUnmarshalled<string, byte[], object>("DownloadBlob", fileName, Data);
+
+	public static async Task LegacyDownloadFile(this IJSRuntime js, string fileName, byte[] Data) =>
+		await js.InvokeAsync<object>("DownloadUrl", fileName, "data:application/octet-stream;base64," + Convert.ToBase64String(Data));
 }
