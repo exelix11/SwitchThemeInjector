87ba45fd5f9725e3c59286911e27fd570969c26d
exelix <ex.elix@libero.it>
2020-03-04 17:19:24 +0100

    Bump theme installer version

--- SwitchThemesNX/source/ViewFunctions.hpp
+++ SwitchThemesNX/source/ViewFunctions.hpp
@@ -7,7 +7,7 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "UI/imgui/imgui_internal.h"
 
-const std::string VersionString = "Ver. 2.2";
+const std::string VersionString = "Ver. 2.2.1";
 extern std::string SystemVer;
 
 void PushPage(IUIControlObj* page);
