f207055e85a7a470dd6149da6c587333f8fcc58c
exelix <ex.elix@libero.it>
2020-08-13 17:47:01 +0200

    Refactor remote installing from theme injector

--- SwitchThemesNX/source/Pages/RemoteInstallPage.hpp
+++ SwitchThemesNX/source/Pages/RemoteInstallPage.hpp
@@ -7,13 +7,8 @@
 #include "../UI/UI.hpp"
 #include "../fs.hpp"
 #include "ThemeEntry/ThemeEntry.hpp"
-#ifdef __SWITCH__
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <fcntl.h>
-#endif
 #include "RemoteInstall/RemoteInstall.hpp"
+#include "../SwitchTools/InjectorInstall.hpp"
 
 class RemoteInstallPage : public IPage
 {
@@ -24,12 +19,9 @@ class RemoteInstallPage : public IPage
 		void Render(int X, int Y) override;
 		void Update() override;
 	private:
-		void StartSocketing();
-		void StopSocketing();
-		void SocketUpdate();
-		
 		void DialogError(const std::string &msg);
 		
+		// Download install
 		int ProviderIndex = 0;
 		const RemoteInstall::Provider& SelectedProvider();
 		std::string RemoteInstallCode;
@@ -37,18 +29,17 @@ class RemoteInstallPage : public IPage
 		void SetRemoteInstallCode(const char* input);
 		void StartRemoteInstallByCode();
 		void StartRemoteInstallFixed(RemoteInstall::FixedTypes type);
-
-		int sock = -1;	
-		//For SocketUpdate:
-		int curSock = -1;
-		u32 ThemeSize = 0;
-		std::vector<u8> data;
 		
-		std::unique_ptr<ThemeEntry> entry = 0;
+		// Injector install
+		InjectorInstall::Server server;
+		void StartServer();
+		void StopServer();
+		void UpdateServer();
+		std::unique_ptr<ThemeEntry> RemoteInstallFile = 0;
 		std::string BtnStart;
-
 		bool AutoInstall = false;
 		
+		// Layout stuff
 		void CurItemBlockLeft();
 		bool AllowLeft = true;
 };
