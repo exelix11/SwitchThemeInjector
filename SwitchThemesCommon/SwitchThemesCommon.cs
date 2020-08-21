b76e296e92defa72bd18ea1d0071f0ec8bfa4d69
exelix <13405476+exelix11@users.noreply.github.com>
2020-08-21 23:36:41 +0200

    Bump version

--- SwitchThemesCommon/SwitchThemesCommon.cs
+++ SwitchThemesCommon/SwitchThemesCommon.cs
@@ -15,8 +15,8 @@ namespace SwitchThemes.Common
 {
 	public static class SwitchThemesCommon
 	{
-		public const string CoreVer = "4.5";
-		public const int NxThemeFormatVersion = 13;
+		public const string CoreVer = "4.5.1";
+		public const int NxThemeFormatVersion = 14;
 
 		public static Dictionary<string, string> PartToFileName = new Dictionary<string, string>() {
 			{"home","ResidentMenu.szs"},
