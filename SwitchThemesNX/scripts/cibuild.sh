7717d5759180199793fd6dc0f23866b9bfdda10b
exelix <ex.elix@libero.it>
2020-09-25 11:02:23 +0200

    User proper checkout action

--- SwitchThemesNX/scripts/cibuild.sh
+++ SwitchThemesNX/scripts/cibuild.sh
@@ -4,6 +4,6 @@ set -e
 # Remove the object file containing the version string
 rm build/Version.o
 
-ver=$(git describe --always --abbrev=40 --dirty)
+ver=$GITHUB_SHA
 make all "GITVER=CI-${ver}" $@
 7z a -y -mx=7 Built.7z SwitchThemesNX.nro SwitchThemesNX.elf
