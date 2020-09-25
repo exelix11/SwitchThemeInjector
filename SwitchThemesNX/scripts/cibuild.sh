0a016cac59710963df26b6af56e636fecd2e47cd
exelix <13405476+exelix11@users.noreply.github.com>
2020-09-25 11:14:02 +0200

    Update cibuild.sh

--- SwitchThemesNX/scripts/cibuild.sh
+++ SwitchThemesNX/scripts/cibuild.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-# Remove the object file containing the version string
-rm build/Version.o
-
 ver=$GITHUB_SHA
 make all "GITVER=CI-${ver}" $@
 7z a -y -mx=7 Built.7z SwitchThemesNX.nro SwitchThemesNX.elf
