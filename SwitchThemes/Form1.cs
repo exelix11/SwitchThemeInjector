215182de83e33c26d38d772591dcaaebc88f9647
exelix11 <13405476+exelix11@users.noreply.github.com>
2020-09-28 14:14:44 +0200

    Clean up dependencies for the theme injector, properly restore packages before build

--- SwitchThemes/Form1.cs
+++ SwitchThemes/Form1.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json;
 using SARCExt;
 using SwitchThemes.Common;
 using SwitchThemes.Common.Bntxx;
