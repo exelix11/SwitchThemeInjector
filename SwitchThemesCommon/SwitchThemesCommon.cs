383d4f4743d576cae85828160f5896381228d90b
exelix11 <ex.elix@libero.it>
2021-10-29 12:37:08 +0200

    Bump injector version

--- SwitchThemesCommon/SwitchThemesCommon.cs
+++ SwitchThemesCommon/SwitchThemesCommon.cs
@@ -15,7 +15,7 @@ namespace SwitchThemes.Common
 {
 	public static class Info
 	{
-		public const string CoreVer = "4.6.2";
+		public const string CoreVer = "4.6.4";
 		public const int NxThemeFormatVersion = 15;
 
 		public static Dictionary<string, string> PartToFileName = new Dictionary<string, string>() {
